5775f1ab375356930ee7255ca3fcec58fd6a3e15
EvcazinDeveloper <93777238+EvcazinDeveloper@users.noreply.github.com>
2023-08-18 17:44:29 -0300

    react eu te odeio

---
+++ config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBAUYaicsjcUJZYKmmB56D6-sxqyzWtPjo",
+    authDomain: "espectograma-460d5.firebaseapp.com",
+    projectId: "espectograma-460d5",
+    storageBucket: "espectograma-460d5.appspot.com",
+    messagingSenderId: "797095999677",
+    appId: "1:797095999677:web:593bb32c55c6cf6b6b1fd4"
+  };
+  
